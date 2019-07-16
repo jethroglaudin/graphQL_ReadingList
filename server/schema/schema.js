72108e8a7102630718a7fbacbbb877809f2fd658
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-16 18:12:48 -0400

    fixed some errors. One with the _ variable that requires lodash and the other in the return for resolve

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,19 @@
 const graphql = require("graphql");
-const_ = require('lodash');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 // dummy data
 
 var books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" }
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
 ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
-  fields: () => ({
+  fields: ( ) => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
@@ -28,7 +28,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // code to get data from db / other source
-       return _find(books,{id: args.id});
+       return _.find(books, {id: args.id});
       }
     }
   }
