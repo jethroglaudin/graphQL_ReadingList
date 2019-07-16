d035c83ce8de56f39c865197bd2081f091953ca3
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-16 16:09:12 -0400

    imported schema

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 const port = process.env.Port || 4000;
 
 app.use('/graphql', graphqlHTTP({
-
+    schema
 }));
 
 app.listen(port,() =>{
