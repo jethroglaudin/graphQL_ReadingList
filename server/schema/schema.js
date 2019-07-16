10c133ac35cf00ff30917b89537dd7bb79694cb0
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-16 18:19:57 -0400

    brought in GraphQLID from graphql. Changed the id type within fields and args to type GraphQLID

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,20 +1,25 @@
 const graphql = require("graphql");
-const _ = require('lodash');
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { 
+    GraphQLObjectType, 
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID 
+} = graphql;
 
 // dummy data
 
 var books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" }
 ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
-  fields: ( ) => ({
-    id: { type: GraphQLString },
+  fields: () => ({
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
@@ -25,15 +30,15 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db / other source
-       return _.find(books, {id: args.id});
+        console.log(typeof args.id);
+        return _.find(books, { id: args.id });
       }
     }
   }
 });
-
 
 module.exports = new GraphQLSchema({
   query: RootQuery
