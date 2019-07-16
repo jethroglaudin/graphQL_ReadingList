9dfef551acaeaea09dac7c44bf413bf9449f1813
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-16 18:52:11 -0400

    type relations)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -12,9 +12,9 @@ const {
 // dummy data
 
 var books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" }
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' }
 ];
 
 var authors = [
@@ -22,14 +22,21 @@ var authors = [
     {name: 'Brandon Sanderson', age: 42, id: '2'},
     {name: 'Terry Pratchett', age: 66, id: '3'},
     
-]
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+        type: AuthorType,
+        resolve(parent, args){
+            console.log(parent);
+            return _.find(authors, {id: parent.authorId})
+        }
+    }
   })
 });
 
