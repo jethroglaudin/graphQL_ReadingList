f08a89c66df8370b809be32221848db3e690275b
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-20 01:29:33 -0400

    removed getBooksQuery and gql

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,32 +1,23 @@
 import React, { Component } from "react";
-import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
-
-
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`;
+import { getAuthorsQuery } from "../queries/queries";
 
 class AddBook extends Component {
-    displayAuthors() {
-        let data = this.props.data;
-        if(data.loading) {
-          return (<option disabled>Loading Authors...</option>);
-        } else {
-          return data.authors.map(author => {
-            return (<option key={author.id} value={author.id}>{ author.name } </option>)
-          })
-        }
-
+  displayAuthors() {
+    let data = this.props.data;
+    if (data.loading) {
+      return <option disabled>Loading Authors...</option>;
+    } else {
+      return data.authors.map(author => {
+        return (
+          <option key={author.id} value={author.id}>
+            {author.name}{" "}
+          </option>
+        );
+      });
     }
-    
-    
+  }
+
   render() {
     return (
       <form id="add-book">
