3c4ebb4a651de6a93610eb82214e4096a8dd9d7e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-19 21:31:43 -0400

    imported BookList component to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
+// Components
+// import BookList from './components/BookList';
+import BookList from './components/BookList';
+
 class App extends Component {
   render() {
     return (
       <div id="main">
         <h1>Ninja's Reading List</h1>
+        <BookList />
+       
       </div>
     );
   }
