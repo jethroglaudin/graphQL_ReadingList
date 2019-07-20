775c2062060de570ef1c7ca6bf10c31bb1708723
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-19 22:18:58 -0400

    updated App.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,11 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cors = require('cors');
 const app = express();
+
+// allow cross-origin request
+app.use(cors());
 const port = process.env.Port || 4000;
 
 mongoose.connect(keys.mongoURL, { useNewUrlParser: true });
